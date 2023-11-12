62d273cc5488c6a301c5354cd4b1c2e89f476e44
POPOcard <123038500+POPOcard@users.noreply.github.com>
2023-11-12 16:16:57 +0800

    Create wxllq.js

---
+++ wxllq.js
@@ -0,0 +1,1 @@
+window.onload=function(){var a=document.getElementById("floating-container");if(isWeixinBrowser()){a.style.border="1px solid black"}};function isWeixinBrowser(){var a=navigator.userAgent.toLowerCase();return a.indexOf("micromessenger")!==-1};
